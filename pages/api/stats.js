9f849e18055e63d49454e13bb212dff21b3f3e79
barneyjeffries <barney@brightwhitespace.co.uk>
2024-02-07 14:34:16 +0000

    remove dev code

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,13 +1,5 @@
 import stats from '../../data/stats.json';
 
-// const handler = nc()
-    // use connect based middleware
-    // .use(cors())
-    // .post(async (req, res) => {
-    //     const response = await fetch('https://wakatime.com/share/@3e8fbf40-286b-4840-a1ad-60245b86b323/3ac817d9-f38d-4fac-9bb7-dac060a09b52.json', config);
-    //     res.json(response);
-    // });
-
 export default function handler(req, res) {
     res.status(200).json(stats.data)
 }
